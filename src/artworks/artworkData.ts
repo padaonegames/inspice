dde28b574002a94296a076adb2f38dc6b1be1d8f
mohrezaei <mrezaei@ucm.es>
2021-05-25 15:32:10 +0200

    IMMA's artworks added

--- src/artworks/artworkData.ts
+++ src/artworks/artworkData.ts
@@ -10,6 +10,19 @@ import venus_adonis_cupido_src from "./venus_adonis_cupido.jpg";
 import susana_src from "./susana.jpg";
 import ofrenda_venus_src from "./ofrenda_venus.jpg";
 import bacanal_adrios_src from "./bacanal_adrios.jpg";
+import little_miss_muffet_src from "./little_miss_muffet.jpg";
+import magnificent_seven_src from "./magnificent_seven.jpg";
+import the_bakers_wife_src from "./the_bakers_wife.jpg";
+import head_of_a_girl_src from "./head_of_a_girl.jpg";
+import the_night_crow_src from "./the_night_crow.jpg";
+import albie_src from "./albie.jpg";
+import pirates_taking_away_children_src from "./pirates_taking_away_children.jpg";
+import the_painters_mother_resting_src from "./the_painters_mother_resting.jpg";
+import ii_trovatore_src from "./ii_trovatore.jpg";
+import swarms_the_moth_src from "./swarms_the_moth.jpg";
+import la_forma_deforme_src from "./la_forma_deforme.jpg";
+import cape_siren_src from "./cape_siren.jpg";
+import boy_src from "./boy.jpg";
 
 export interface ArtworkData {
   src: string; // path de la imagen a usar
@@ -20,138 +33,281 @@ export interface ArtworkData {
   date: string; // año o época en la que se creó
   location: string; // museo y sala donde se encuentra la obra
   info: string; // cualquier otro tipo de info adicional
-};
+}
 
 const vulcano: ArtworkData = {
   src: vulcano_src,
-  id: 'vulcano',
-  title: 'La fragua de vulcano',
+  id: "vulcano",
+  title: "La fragua de vulcano",
   title_en: `Vulcan's forge`,
-  author: 'Velázquez, Diego Rodríguez de Silva y',
-  info: 'Óleo sobre lienzo, 223 x 290 cm',
-  location: 'Museo del Prado Sala 011',
-  date: '1630'
+  author: "Velázquez, Diego Rodríguez de Silva y",
+  info: "Óleo sobre lienzo, 223 x 290 cm",
+  location: "Museo del Prado Sala 011",
+  date: "1630",
 };
 
 const arachne: ArtworkData = {
   src: arachne_src,
-  id: 'arachne',
-  title: 'Las hilanderas o la fábula de Aracne',
+  id: "arachne",
+  title: "Las hilanderas o la fábula de Aracne",
   title_en: `The Spinners, or the Fable of Arachne`,
-  author: 'Velázquez, Diego Rodríguez de Silva y',
-  info: 'Óleo sobre lienzo, 220 x 289 cm',
-  location: 'Museo del Prado Sala C',
-  date: '1655 - 1660'
+  author: "Velázquez, Diego Rodríguez de Silva y",
+  info: "Óleo sobre lienzo, 220 x 289 cm",
+  location: "Museo del Prado Sala C",
+  date: "1655 - 1660",
 };
 
 const judit: ArtworkData = {
   src: judit_src,
-  id: 'judit',
-  title: 'Judit en el banquete de Holofernes',
+  id: "judit",
+  title: "Judit en el banquete de Holofernes",
   title_en: `Judith at the Banquet of Holofernes`,
-  author: 'Rembrandt Harmensz. van Rijn',
-  info: 'Óleo sobre lienzo, 143 x 154,7 cm',
-  location: 'Museo del Prado Sala 076',
-  date: '1634'
+  author: "Rembrandt Harmensz. van Rijn",
+  info: "Óleo sobre lienzo, 143 x 154,7 cm",
+  location: "Museo del Prado Sala 076",
+  date: "1634",
 };
 
 const proserpina: ArtworkData = {
   src: proserpina_src,
-  id: 'proserpina',
-  title: 'El rapto de Proserpina',
+  id: "proserpina",
+  title: "El rapto de Proserpina",
   title_en: `The Rape of Proserpine`,
-  author: 'Rubens, Pedro Pablo',
-  info: 'Óleo sobre lienzo, 181 x 271,2cm',
-  location: 'Museo del Prado Sala 079',
-  date: '1636 - 1637'
+  author: "Rubens, Pedro Pablo",
+  info: "Óleo sobre lienzo, 181 x 271,2cm",
+  location: "Museo del Prado Sala 079",
+  date: "1636 - 1637",
 };
 
 const hipodamia: ArtworkData = {
   src: hipodamia_src,
-  id: 'hipodamia',
-  title: 'El rapto de Hipodamía',
+  id: "hipodamia",
+  title: "El rapto de Hipodamía",
   title_en: `The Rape of Hippodamia`,
-  author: 'Rubens, Pedro Pablo',
-  info: 'Óleo sobre lienzo, 182,5 x 285,5 cm',
-  location: 'Museo del Prado Sala 079',
-  date: '1636 - 1637'
+  author: "Rubens, Pedro Pablo",
+  info: "Óleo sobre lienzo, 182,5 x 285,5 cm",
+  location: "Museo del Prado Sala 079",
+  date: "1636 - 1637",
 };
 
 const diana: ArtworkData = {
   src: diana_src,
-  id: 'diana',
-  title: 'Diana y Calisto',
+  id: "diana",
+  title: "Diana y Calisto",
   title_en: `Diana and Callisto`,
-  author: 'Rubens, Pedro Pablo',
-  info: 'Óleo sobre lienzo, 202,6 x 325,5 cm',
-  location: 'Museo del Prado Sala C',
-  date: 'Hacia 1635'
+  author: "Rubens, Pedro Pablo",
+  info: "Óleo sobre lienzo, 202,6 x 325,5 cm",
+  location: "Museo del Prado Sala C",
+  date: "Hacia 1635",
 };
 
 const hipomenes_atalanta: ArtworkData = {
   src: hipomenes_atalanta_src,
-  id: 'hipomenes_atalanta',
-  title: 'Hipómenes y Atalanta',
+  id: "hipomenes_atalanta",
+  title: "Hipómenes y Atalanta",
   title_en: `Hippomenes and Atalanta`,
-  author: 'Reni, Guido',
-  info: 'Óleo sobre lienzo, 206 x 297 cm',
-  location: 'Museo del Prado, No expuesto',
-  date: '1618 - 1619'
+  author: "Reni, Guido",
+  info: "Óleo sobre lienzo, 206 x 297 cm",
+  location: "Museo del Prado, No expuesto",
+  date: "1618 - 1619",
 };
 
 const venus_adonis: ArtworkData = {
   src: venus_adonis_src,
-  id: 'venus_adonis',
-  title: 'Venus y Adonis',
+  id: "venus_adonis",
+  title: "Venus y Adonis",
   title_en: `Venus and Adonis`,
-  author: 'Veronés, Paolo',
-  info: 'Óleo sobre lienzo, 162 x 191 cm',
-  location: 'Museo del Prado, Sala C',
-  date: 'Hacia 1580'
+  author: "Veronés, Paolo",
+  info: "Óleo sobre lienzo, 162 x 191 cm",
+  location: "Museo del Prado, Sala C",
+  date: "Hacia 1580",
 };
 
 const venus_adonis_cupido: ArtworkData = {
   src: venus_adonis_cupido_src,
-  id: 'venus_adonis_cupido',
-  title: 'Venus, Adonis y Cupido',
+  id: "venus_adonis_cupido",
+  title: "Venus, Adonis y Cupido",
   title_en: `Venus, Adonis and Cupid`,
-  author: 'Carracci, Annibale',
-  info: 'Óleo sobre lienzo, 212 x 268 cm',
-  location: 'Museo del Prado, Sala 026',
-  date: 'Hacia 1590'
+  author: "Carracci, Annibale",
+  info: "Óleo sobre lienzo, 212 x 268 cm",
+  location: "Museo del Prado, Sala 026",
+  date: "Hacia 1590",
 };
 
 const susana: ArtworkData = {
   src: susana_src,
-  id: 'susana',
-  title: 'Susana y los viejos',
+  id: "susana",
+  title: "Susana y los viejos",
   title_en: `Susannah and the Elders`,
-  author: 'Guercino',
-  info: 'Óleo sobre lienzo, 176 x 208 cm',
-  location: 'Museo del Prado, Sala 005',
-  date: '1617'
+  author: "Guercino",
+  info: "Óleo sobre lienzo, 176 x 208 cm",
+  location: "Museo del Prado, Sala 005",
+  date: "1617",
 };
 
 const ofrenda_venus: ArtworkData = {
   src: ofrenda_venus_src,
-  id: 'ofrenda_venus',
-  title: 'Ofrenda a Venus',
+  id: "ofrenda_venus",
+  title: "Ofrenda a Venus",
   title_en: `The Worship of Venus`,
-  author: 'Tiziano, Vecellio di Gregorio',
-  info: 'Óleo sobre lienzo, 172 x 175 cm',
-  location: 'Museo del Prado, Sala C',
-  date: '1518'
+  author: "Tiziano, Vecellio di Gregorio",
+  info: "Óleo sobre lienzo, 172 x 175 cm",
+  location: "Museo del Prado, Sala C",
+  date: "1518",
 };
 
 const bacanal_adrios: ArtworkData = {
   src: bacanal_adrios_src,
-  id: 'bacanal_adrios',
-  title: 'La bacanal de los andrios',
+  id: "bacanal_adrios",
+  title: "La bacanal de los andrios",
   title_en: `The Andrians`,
-  author: 'Tiziano, Vecellio di Gregorio',
-  info: 'Óleo sobre lienzo, 175 x 193 cm',
-  location: 'Museo del Prado, Sala C',
-  date: '1523 - 1526'
+  author: "Tiziano, Vecellio di Gregorio",
+  info: "Óleo sobre lienzo, 175 x 193 cm",
+  location: "Museo del Prado, Sala C",
+  date: "1523 - 1526",
+};
+
+const little_miss_muffet: ArtworkData = {
+  src: little_miss_muffet_src,
+  id: "little_miss_muffet",
+  title: "Little Miss Muffet",
+  title_en: `Little Miss Muffet`,
+  author: "Paula, Rego",
+  info: "Etching and aquatint on hand-made paper, 52 x 38 cm",
+  location: "	IMMA Collection: Purchase, 1996 Not on view",
+  date: "1989",
+};
+
+const magnificent_seven: ArtworkData = {
+  src: magnificent_seven_src,
+  id: "magnificent_seven",
+  title: "Magnificent Seven",
+  title_en: `Magnificent Seven`,
+  author: "Ellen, Gallagher",
+  info: "Ink and paper on canvas, 213 x 183 cm",
+  location: "	IMMA Collection: Purchase, 1997. Not on view",
+  date: "1997",
+};
+
+const the_bakers_wife: ArtworkData = {
+  src: the_bakers_wife_src,
+  id: `the_baker's_wife`,
+  title: `The Baker's Wife`,
+  title_en: `The Baker's Wife`,
+  author: "Paula, Rego",
+  info: "Etching and aquatint on hand-made paper, 33.5 x 26.3 cm",
+  location: "IMMA Collection: Purchase, 1997. Not on view",
+  date: "1989",
+};
+
+const head_of_a_girl: ArtworkData = {
+  src: head_of_a_girl_src,
+  id: "head_of_a_girl",
+  title: "Head of a Girl",
+  title_en: "Head of a Girl",
+  author: "Lucian, Freud",
+  info: "Oil on canvas, 50.8 x 40.6 cm",
+  location: "	IMMA Collection: On Loan. Not on view",
+  date: "1975",
+};
+
+const the_night_crow: ArtworkData = {
+  src: the_night_crow_src,
+  id: "the_night_crow",
+  title: "The Night Crow",
+  title_en: "The Night Crow",
+  author: "Paula, Rego",
+  info: "Etching on hand-made paper, 28 x 18 cm",
+  location: "IMMA Collection: Purchase, 1997. Not on view",
+  date: "1992",
+};
+
+const albie: ArtworkData = {
+  src: albie_src,
+  id: "albie",
+  title: "Albie",
+  title_en: "Albie",
+  author: "Lucian, Freud",
+  info: "Oil on canvas, 36.8 x 30.5 cm",
+  location: "	IMMA Collection: On Loan. Not on view",
+  date: "2003",
+};
+
+const pirates_taking_away_children: ArtworkData = {
+  src: pirates_taking_away_children_src,
+  id: "pirates_taking_away_children",
+  title: "Pirates Taking Away Children",
+  title_en: "Pirates Taking Away Children",
+  author: "Paula, Rego",
+  info: "	Etching and aquatint on hand-made paper, 28 x 20 cm",
+  location: "	IMMA Collection: Purchase, 1996. Not on view",
+  date: "1992",
+};
+
+const the_painters_mother_resting: ArtworkData = {
+  src: the_painters_mother_resting_src,
+  id: "the_painters_mother_resting",
+  title: `The Painter's Mother Resting`,
+  title_en: `The Painter's Mother Resting`,
+  author: "Lucian, Freud",
+  info: "Oil on canvas, 90.2 x 90.2 cm",
+  location: "	IMMA Collection: On Loan. Not on view",
+  date: "1976",
+};
+
+const ii_trovatore: ArtworkData = {
+  src: ii_trovatore_src,
+  id: "ii_trovatore",
+  title: "II Trovatore",
+  title_en: "II Trovatore",
+  author: "Giorgio, de Chirico",
+  info: "Oil on canvas, 60 x 48 cm",
+  location: "	IMMA Collection: Heritage Gift. Not on view",
+  date: "1955",
+};
+
+const swarms_the_moth: ArtworkData = {
+  src: swarms_the_moth_src,
+  id: "swarms_the_moth",
+  title: "Swarms the Moth",
+  title_en: "Swarms the Moth",
+  author: "Patrick Michael, Fitzgerald",
+  info: "Oil and collage on linen, 160 x 180 cm",
+  location: "IMMA Collection: Purchase, 2010. Not on view",
+  date: "2010",
+};
+
+const la_forma_deforme: ArtworkData = {
+  src: la_forma_deforme_src,
+  id: "la_forma_deforme",
+  title: "La Forma Deforme",
+  title_en: "La Forma Deforme",
+  author: "Luis, López Loza",
+  info: "Lithograph, 60 x 50 cm",
+  location: "IMMA Collection: Diplomatic Gift, 2011. Not on view",
+  date: "2010",
+};
+
+const cape_siren: ArtworkData = {
+  src: cape_siren_src,
+  id: "cape_siren",
+  title: "Cape Siren",
+  title_en: "Cape Siren",
+  author: "Philip, Taaffe",
+  info: "Mixed media on linen, 297.8 x 246.7 cm",
+  location: "	IMMA Collection: Heritage Gift. Not on view",
+  date: "2008",
+};
+
+const boy: ArtworkData = {
+  src: boy_src,
+  id: "boy",
+  title: "Boy",
+  title_en: "Boy",
+  author: "Geraldine, O'Neill",
+  info: "Oil on canvas, 240 x 130 cm",
+  location: "	IMMA Collection: Donation. Not on view",
+  date: "2008",
 };
 
 export const sampleArtworks: ArtworkData[] = [
@@ -167,4 +323,17 @@ export const sampleArtworks: ArtworkData[] = [
   susana,
   ofrenda_venus,
   bacanal_adrios,
+  little_miss_muffet,
+  magnificent_seven,
+  the_bakers_wife,
+  head_of_a_girl,
+  the_night_crow,
+  albie,
+  pirates_taking_away_children,
+  the_painters_mother_resting,
+  ii_trovatore,
+  swarms_the_moth,
+  la_forma_deforme,
+  cape_siren,
+  boy,
 ];
