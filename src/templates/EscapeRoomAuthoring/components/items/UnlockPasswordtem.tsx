c4829cdc00ec5d4dc8b999862124601e1336f2ea
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2022-07-06 13:53:14 +0200

    Created unlock password item

--- src/templates/EscapeRoomAuthoring/components/items/UnlockPasswordtem.tsx
+++ src/templates/EscapeRoomAuthoring/components/items/UnlockPasswordtem.tsx
@@ -1,8 +1,31 @@
-import styled from "styled-components";
 import { EditableItemProps, MultipleChoiceItemDefinition, NarrativeItemDefinition, UnlockPasswordItemDefinition } from "../../../../services/escapeRoomActivity.model";
-import EditableCheckBoxInput from "../EditableCheckBoxInput";
 import { AbstractActivityItemFactory } from "../ActivityItemFactory";
 import { PromptField } from "./PromptField";
+
+import styled from "styled-components";
+import {UpArrow} from "@styled-icons/boxicons-solid/UpArrow"
+import {DownArrow} from "@styled-icons/boxicons-solid/DownArrow"
+import {DotsTwoVertical} from "@styled-icons/entypo/DotsTwoVertical"
+
+const UppArrowIcon = styled(UpArrow)`
+  position: absolute;
+  color: rgb(0, 0, 0);
+  height: 50px;
+  top:-25%;
+`
+const DownArrowIcon = styled(DownArrow)`
+  position: absolute;
+  color: rgb(0, 0, 0);
+  height: 50px;
+  bottom:-25%;
+`
+const DotsTwoVerticalIcon = styled(DotsTwoVertical)`
+  position: relative;
+  margin-left:-50px;
+  margin-right:-50px;
+  color: rgb(0, 0, 0);
+  height: 50%;
+`
 
 interface InputAreaProps {
   width?: string;
@@ -35,77 +58,124 @@ export const InputArea = styled.textarea<InputAreaProps>`
   }
 `;
 
-const CheckboxList = styled.div`
+const Root = styled.div`
   margin-top: 5px;
   display: flex;
   background-color: rgba(255,255,0,0.5);
   flex-direction: column;
-  align-items: left;
-
+  align-items: center;
+  text-align:center;
   border-bottom: 2px solid #dadce0;
   padding: 0.75em;
-
+  
   background-color:  #dbdbdb;
-
+  
   border-radius: 1.25rem;
   box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
 `;
 
-interface CheckBoxOptionProps {
-  backgroundColor?: string;
-}
-const CheckboxOption = styled.div<CheckBoxOptionProps>`
-  margin-top: 0.25em;
-  margin-bottom: 0.25em;
-  padding: 0.75em 0 0.75em 0.75em;
-  border-top: none;
-  font-size: 1em;
-  font-weight: 200;
-  font-family: ${props => props.theme.contentFont};
-  color: white;
-  line-height: 135%;
-  width: 100%;
+const DescriptionEditorContainer = styled.div`
+  position: relative;
+  width: 50%;
   display: flex;
-  flex-direction: row;
-  align-content: center;
-
-  background-color: ${props => props.backgroundColor || 'transparent'};
-
-  border-radius: 0.25rem;
+  flex-direction:column;
+  align-items: center;
+  text-align: center;
+  background-color: rgba(200,200,200,1);
+  border-radius:1rem 1rem 0 0;
   box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
 `;
 
-const PreviewTitle = styled.div`
-  margin-bottom: 0.25rem;
-  color: rgb(110, 110, 110);
-  text-align: center;
-  font-size: 0.75rem;
-  line-height: 1.33;
-  letter-spacing: 0.2px;
-  max-height: 1.5rem;
-  max-width: 100%;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
+const DescriptionEditorTitle = styled.div`
+  position: relative;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: ${props => props.theme.contentFont};
+  line-height: 135%;
 
-const PreviewAnswers = styled.div`
+  margin-bottom: 0.25em;
+  border-top: none;
+  color: black;
+  line-height: 135%;
+  
+  height:50%;
+  
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-top: 3px;
-  color: rgb(178, 178, 178);
+  text-align: center;
+  padding:1rem;
+  margin-top:10px;
+  border-radius:1rem;
+
+  background-color: rgba(255,255,255,1);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
 `;
 
-const PreviewAnswer = styled.div`
+const PasswordEditorContainer = styled.div`
   position: relative;
-  width: calc(100% - 0.125rem);
-  height: 7px;
-  margin-bottom: 3px;
-  border: 1px solid rgb(229, 229, 229);
-  border-radius: 0.125rem;
+  width: 50%;
+  height:400px;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  text-align: center;
+  border-radius: 0 0 1rem 1rem;
+  background-color: rgba(150,150,150,1);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
 `;
+
+const PasswordEditorTitle = styled.div`
+  position: relative;
+  height:10%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  padding:1rem;
+  margin-top:5px;
+  border-radius:1rem;
+  background-color: rgba(255,255,255,1);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
+`;
+
+const PasswordEditorDigits = styled.div`
+  position: relative;
+  width: 90%;
+  height:90%;
+  display: flex;
+  flex-direction:row;
+  justify-content: center;
+  align-items:center;
+  text-align: center;
+  // background-color: rgba(0,0,255,0.5);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
+`;
+
+const DigitEditor = styled.div`
+  position: relative;
+  width: 20%;
+  height:50%;
+  display: flex;
+  flex-direction:column;
+  align-items: center;
+  justify-content:center;
+  text-align: center;
+  background-color: rgba(255,255,255,1);
+  margin-left: 5px;
+  margin-right:5px;
+  // border-color: rgba(0,0,0,1);
+  border: 1px solid #000;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px -4px 0px 0px inset;
+`;
+
+const Digit = styled.p`
+  font-weight: 400;
+  font-size:120px;
+  line-height: 1.5;
+  transition: color 0.5s ease;
+  word-wrap: break-word;
+`;
+
+
 
 export interface EditableUnlockPasswordItemContentProps extends EditableItemProps<UnlockPasswordItemDefinition> {
   /** text to display for the add new option label. */
@@ -125,47 +195,87 @@ export const EditableUnlockPasswordItemContent = (props: EditableUnlockPasswordI
     password
   } = payload;
 
-  // const handleAddOption = () => {
-  //   if (!onPayloadChanged) return;
-  //   onPayloadChanged({
-  //     ...payload,
-  //     dialogs: [...payload.dialogs, '']
-  //   })
-  // }; // handleAddOption
+  const handleEditDigit = (index: number, value: number) => {
+    if (!onPayloadChanged) return;
+    onPayloadChanged({
+      ...payload,
+      password: [
+        ...payload.password.slice(0, index),
+        value,
+        ...payload.password.slice(index + 1)
+      ]
+    })
+  }; // handleEditOption
 
-  // const handleEditOption = (index: number, value: string) => {
-  //   if (!onPayloadChanged) return;
-  //   onPayloadChanged({
-  //     ...payload,
-  //     dialogs: [
-  //       ...payload.dialogs.slice(0, index),
-  //       value,
-  //       ...payload.dialogs.slice(index + 1)
-  //     ]
-  //   })
-  // }; // handleEditOption
-
-  // const handleRemoveOption = (index: number) => {
-  //   if (!onPayloadChanged) return;
-  //   onPayloadChanged({
-  //     ...payload,
-  //     dialogs: payload.dialogs.filter((_, i) => i !== index)
-  //   })
-  // }; // handleRemoveOption
-
-
-
-
-  const availableColors = ['#e21b3c', '#1368ce', '#d89e00', '#26890c', '#0aa3a3', '#864cbf'];
+  const handleEditDescription = ( value: string) => {
+    if (!onPayloadChanged) return;
+    onPayloadChanged({
+      ...payload,
+      description: value
+    })
+  }; // handleEditOption
 
   return (
     <>
-    <CheckboxList>
-      <PromptField promptText={""} promptPlaceholder='Start typing your prompt' onPromptChange={()=>{}}/>
-    </CheckboxList>
+    <Root>
+      <DescriptionEditorContainer>
+        <DescriptionEditorTitle> Description to help find out the password </DescriptionEditorTitle>
+        <PromptField promptText={description} promptPlaceholder='Start typing your prompt' onPromptChange={(value)=>{handleEditDescription(value)}}/>
+      </DescriptionEditorContainer>
+
+      <PasswordEditorContainer>
+        <PasswordEditorTitle>Password to solve this puzzle</PasswordEditorTitle>
+        <PasswordEditorDigits>
+          <DigitEditor>
+            <UppArrowIcon onMouseDown={()=>{handleEditDigit(0,(password[0]+1)%10)}} />
+            <Digit>{password[0]}</Digit>
+            <DownArrowIcon onMouseDown={()=>{handleEditDigit(0,password[0]-1 < 0 ? 9: password[0]-1)}}/>
+          </DigitEditor>
+          <DigitEditor>
+            <UppArrowIcon  onMouseDown={()=>{handleEditDigit(1,(password[1]+1)%10)}}/>
+            <Digit>{password[1]}</Digit>
+            <DownArrowIcon onMouseDown={()=>{handleEditDigit(1,password[1]-1 < 0 ? 9: password[1]-1)}}/>
+          </DigitEditor>
+          
+          <DotsTwoVerticalIcon/>
+
+          <DigitEditor>
+            <UppArrowIcon  onMouseDown={()=>{handleEditDigit(2,(password[2]+1)%10)}}/>
+            <Digit>{password[2]}</Digit>
+            <DownArrowIcon onMouseDown={()=>{handleEditDigit(2,password[2]-1 < 0 ? 9: password[2]-1)}}/>
+          </DigitEditor>
+          <DigitEditor>
+            <UppArrowIcon  onMouseDown={()=>{handleEditDigit(3,(password[3]+1)%10)}}/>
+            <Digit>{password[3]}</Digit>
+            <DownArrowIcon  onMouseDown={()=>{handleEditDigit(3,password[3]-1 < 0 ? 9: password[3]-1)}}/>
+          </DigitEditor>
+
+        </PasswordEditorDigits>
+      </PasswordEditorContainer>
+    </Root>
     </>
   );
 }; // EditableMultipleChoiceItemContent
+
+
+
+
+
+
+const PreviewTitle = styled.div`
+  margin-bottom: 0.25rem;
+  color: rgb(110, 110, 110);
+  text-align: center;
+  font-size: 0.75rem;
+  line-height: 1.33;
+  letter-spacing: 0.2px;
+  max-height: 1.5rem;
+  max-width: 100%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
 
 export const UnlockPasswordItemStageSlide = (props: UnlockPasswordItemDefinition): JSX.Element => {
 
