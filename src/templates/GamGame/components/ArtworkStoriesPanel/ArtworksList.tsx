5c29d62bdbf1ec48753977e5ca0abe750a2af779
Pablo Gutierrez Sánchez <pabgut02@ucm.es>
2022-09-21 11:34:40 +0200

    Added 'No Artworks Found' text to artworks list when no artworks are available

--- src/templates/GamGame/components/ArtworkStoriesPanel/ArtworksList.tsx
+++ src/templates/GamGame/components/ArtworkStoriesPanel/ArtworksList.tsx
@@ -1,50 +1,54 @@
-import { useState } from 'react';
-import { ArtworkGrid } from '../generalStyles';
-import SearchBar from '../../../../components/Forms/SearchBar';
-import ArtworkColumnElement from '../../../../components/ArtworkSelection/ArtworkColumnElement';
-import { ArtworkData } from '../../../../services/artwork.model';
-import { useTranslation } from 'react-i18next';
+import { useState } from "react";
+import { ArtworkGrid, NoItemsFoundText } from "../generalStyles";
+import SearchBar from "../../../../components/Forms/SearchBar";
+import ArtworkColumnElement from "../../../../components/ArtworkSelection/ArtworkColumnElement";
+import { ArtworkData } from "../../../../services/artwork.model";
+import { useTranslation } from "react-i18next";
 
 export interface ArtworksListProps {
   /** Artworks to be rendered within this component */
   artworks: ArtworkData[];
   /** Callback to parent specifying that a given artwork with artwork has been selected */
   onArtworkSelected?: (artworkId: string) => void;
-};
+}
 
 /** List of arworks to search through. */
 export const ArtworksList = (props: ArtworksListProps): JSX.Element => {
-
-  const { t } = useTranslation('gamGame');
+  const { t } = useTranslation("gamGame");
   const { artworks, onArtworkSelected } = props;
 
-  const [filter, setFilter] = useState<string>('');
+  const [filter, setFilter] = useState<string>("");
 
-  const displayArtworks = artworks.filter(elem =>
-    elem.title.toLowerCase().includes(filter) ||
-    elem.author.toLowerCase().includes(filter)
+  const displayArtworks = artworks.filter(
+    (elem) =>
+      elem.title.toLowerCase().includes(filter) ||
+      elem.author.toLowerCase().includes(filter)
   );
 
   return (
     <>
       <SearchBar
-        placeholder={`${t('searchArtworksByTitleOrAuthor')}...`}
+        placeholder={`${t("searchArtworksByTitleOrAuthor")}...`}
         onSearchPerformed={(search) => setFilter(search)}
       />
-
-      <ArtworkGrid>
-        {displayArtworks.map(elem => (
-          <ArtworkColumnElement
-            key={elem.id}
-            artworkData={elem}
-            onCardClicked={() => {
-              if (onArtworkSelected) {
-                onArtworkSelected(elem.id);
-              }
-            }}
-          />
-        ))}
-      </ArtworkGrid>
+      {displayArtworks.length === 0 && (
+        <NoItemsFoundText>{`${t("noArtworksFound")}`}</NoItemsFoundText>
+      )}
+      {displayArtworks.length > 0 && (
+        <ArtworkGrid>
+          {displayArtworks.map((elem) => (
+            <ArtworkColumnElement
+              key={elem.id}
+              artworkData={elem}
+              onCardClicked={() => {
+                if (onArtworkSelected) {
+                  onArtworkSelected(elem.id);
+                }
+              }}
+            />
+          ))}
+        </ArtworkGrid>
+      )}
     </>
   );
 };
